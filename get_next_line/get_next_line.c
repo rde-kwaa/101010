347ba7a811f693580ae8636fca052fbb715eb854
rde-kwaa <36619374+rde-kwaa@users.noreply.github.com>
2018-06-19 10:39:30 +0200

    added comments

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+// this function finds any newline that may be in the buffer.
 int		end(char *buf)
 {
 	int i;
@@ -28,14 +28,19 @@ int		get_line(const int fd, char *buf, char *strs[fd])
 {
 	int			rn;
 	char		*tmp;
-
+// while loop happens while there is no newline, and while it can read from fd
 	while ((end(buf) != 1) && ((rn = read(fd, buf, BUFF_SIZE)) > 0))
 	{
+		// null terminates for string manipulation (eg, ft_strjoin)
 		buf[rn] = '\0';
+		//read into tmp str (first time this runs the tmp will be empty as strs (static var) is empty
 		tmp = strs[fd];
+		// joins tmp and buf into the global var
 		strs[fd] = ft_strjoin(tmp, buf);
+		// frees up tmp
 		ft_strdel(&tmp);
 	}
+	// frees up buf
 	ft_strdel(&buf);
 	if (rn < 0)
 		return (-1);
@@ -49,22 +54,29 @@ int		get_next_line(const int fd, char **line)
 	char		*buf;
 	char		*str;
 	char		*tmp;
-
+// error checking
 	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	buf = ft_strnew(BUFF_SIZE);
+	// checks if static var is initialised, if not, creates new one.
 	if (!strs[fd])
 		strs[fd] = ft_strnew(0);
+// runs get_line function and sees if it fails
 	if ((rn = get_line(fd, buf, strs)) == -1)
 		return (-1);
+	// after \n is found, find the delimiter ('\n') and puts everything from delim. and after into str
 	if ((str = ft_strchr(strs[fd], '\n')))
 	{
+		// sub string of strs[fd] from the beginning to before the newline
 		*line = ft_strsub(strs[fd], 0, str - strs[fd]);
 		tmp = strs[fd];
+		// allocates memory for everything after the newline
 		strs[fd] = ft_strdup(str + 1);
+		// frees tmp
 		ft_strdel(&tmp);
 		return (1);
 	}
+	// after eveything is finished, puts whatever is left into the line
 	*line = ft_strdup(strs[fd]);
 	ft_strdel(&strs[fd]);
 	return (ft_strlen(*line) > 0 ? 1 : 0);
